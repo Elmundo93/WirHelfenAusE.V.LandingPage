60535e361891246b4b34e758d288e8ebbf9b028e
Lemont-Kim Mrutschock <lemont@MacBook-Air-von-Lemont-Kim.fritz.box>
2025-07-02 19:30:56 +0200

    link adjustment

---
+++ lib/featureIcons.ts
@@ -0,0 +1,17 @@
+import People from '@/public/images/staff-personnel-structure-management-svgrepo-com.svg';
+import SichererRahmen from '@/public/images/safe-and-stable-svgrepo-com.svg';
+import InderNähe from '@/public/images/location-svgrepo-com.svg';
+import Handshake from '@/public/images/handshake-svgrepo-com.svg';
+import Register from '@/public/images/register-svgrepo-com.svg';
+import RightOne from '@/public/images/choice-svgrepo-com.svg';
+
+const featureIcons = {
+  org: People,
+  safe: SichererRahmen,
+  region: InderNähe,
+  respect: Handshake,
+  register: Register,
+  match: RightOne,
+};
+
+export default featureIcons;
